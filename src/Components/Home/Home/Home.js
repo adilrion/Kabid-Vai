fcea13c0b65b4f4e1cbd184e143d90897298422f
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2022-04-12 03:09:30 +0600

    navigation added

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Navigation from "../../Shared/Navigation/Navigation";
 
 const Home = () => {
   return (
-    <div>
-      <h1>this is home page</h1>
-    </div>
+    <>
+      <Navigation />
+    </>
   );
 };
 
