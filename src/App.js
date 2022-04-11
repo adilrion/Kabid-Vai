cd3d4644fab67d5672cb784bb7021ac3218242ae
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2022-04-11 23:48:56 +0600

    create version page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home/Home";
 import Version from "./Components/Home/Version/Version";
+import VersionBangla from "./Components/Home/Version/VersionBangla";
 
 function App() {
   return (
@@ -8,7 +9,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Version />} />
-          <Route path="/" element={<Version />} />
+          <Route path="/" elem ent={<Version />} />
+          <Route path="v-english" element={<Home />} />
+          <Route path="v-bangla" element={<VersionBangla />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/Components/Home/Version/Version.js
+++ src/Components/Home/Version/Version.js
@@ -1,15 +1,43 @@
 import React from "react";
+import "./Version.css";
+import KabidImg from "../../Shared/Images/Kabid.jpg";
+import { Link } from "react-router-dom";
 
 const Version = () => {
   return (
     <section>
-      <div className="min-h-screen min-w-[100vw] flex flex-col ">
-        <div>
-          <h1>Kabid hossain</h1>
-          <p>
+      <div className="min-h-screen min-w-[100vw] flex flex-col justify-center items-center">
+        <div className="text-center">
+          <div className="flex flex-col justify-center items-center">
+            <div className="rainbow">
+              <img
+                src={KabidImg}
+                alt="Images of Kabid Hossain"
+                className="w-[200px] h-[200px] text-center"
+              />
+            </div>
+            <h1>Kabid hossain</h1>
+          </div>
+          <p className="md:w-[400px] py-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
             quas!
           </p>
+        </div>
+        <div className="mt-4">
+          <Link
+            to="/v-english"
+            type="button"
+            className="text-gray-800 border border-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2 mr-2 mb-2"
+          >
+            English Version
+          </Link>
+          <Link
+            to="/v-bangla"
+            type="button"
+            className="text-gray-800 border border-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2 mr-2 mb-2"
+          >
+            Bangla Version
+          </Link>
         </div>
       </div>
     </section>
