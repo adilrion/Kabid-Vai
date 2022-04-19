709253441b3aa39662f1b90f55671b8829f18b35
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2022-04-20 04:23:13 +0600

    works section image viewer npm added

--- src/Components/Home/Works/Works.js
+++ src/Components/Home/Works/Works.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import { Zoom } from "react-reveal";
+import Viewer from "react-viewer";
+
 import "./Works.css";
 
 // import { BsSearch } from "react-icons/bs";
@@ -9,71 +11,73 @@ const library = [
   {
     id: "1",
     title: "A",
-    img: "https://media.istockphoto.com/photos/freedom-chains-that-transform-into-birds-charge-concept-picture-id1322104312?b=1&k=20&m=1322104312&s=170667a&w=0&h=VQyPkFkMKmo0e4ixjhiOLjiRs_ZiyKR_4SAsagQQdkk=",
+    src: "https://media.istockphoto.com/photos/freedom-chains-that-transform-into-birds-charge-concept-picture-id1322104312?b=1&k=20&m=1322104312&s=170667a&w=0&h=VQyPkFkMKmo0e4ixjhiOLjiRs_ZiyKR_4SAsagQQdkk=",
   },
   {
     id: "2",
     title: "V",
-    img: "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
+    src: "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
   },
   {
     id: "3",
     title: "X",
-    img: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bWFufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    src: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bWFufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
   },
   {
     id: "4",
     title: "E",
-    img: "https://images.freeimages.com/images/large-previews/5a3/flowers-1375015.jpg",
+    src: "https://images.freeimages.com/images/large-previews/5a3/flowers-1375015.jpg",
   },
   {
     id: "5",
     title: "Z",
-    img: "https://images.freeimages.com/images/large-previews/5a3/flowers-1375015.jpg",
+    src: "https://images.freeimages.com/images/large-previews/5a3/flowers-1375015.jpg",
   },
   {
     id: "6",
     title: "Q",
-    img: "https://media.istockphoto.com/photos/freedom-chains-that-transform-into-birds-charge-concept-picture-id1322104312?b=1&k=20&m=1322104312&s=170667a&w=0&h=VQyPkFkMKmo0e4ixjhiOLjiRs_ZiyKR_4SAsagQQdkk=",
+    src: "https://media.istockphoto.com/photos/freedom-chains-that-transform-into-birds-charge-concept-picture-id1322104312?b=1&k=20&m=1322104312&s=170667a&w=0&h=VQyPkFkMKmo0e4ixjhiOLjiRs_ZiyKR_4SAsagQQdkk=",
   },
   {
     id: "7",
     title: "S",
-    img: "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
+    src: "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
   },
   {
     id: "8",
     title: "S",
-    img: "https://media.istockphoto.com/photos/freedom-chains-that-transform-into-birds-charge-concept-picture-id1322104312?b=1&k=20&m=1322104312&s=170667a&w=0&h=VQyPkFkMKmo0e4ixjhiOLjiRs_ZiyKR_4SAsagQQdkk=",
+    src: "https://media.istockphoto.com/photos/freedom-chains-that-transform-into-birds-charge-concept-picture-id1322104312?b=1&k=20&m=1322104312&s=170667a&w=0&h=VQyPkFkMKmo0e4ixjhiOLjiRs_ZiyKR_4SAsagQQdkk=",
   },
   {
     id: "9",
     title: "S",
-    img: "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
+    src: "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
   },
   {
     id: "10",
     title: "S",
-    img: "https://images.freeimages.com/images/large-previews/5a3/flowers-1375015.jpg",
+    src: "https://images.freeimages.com/images/large-previews/5a3/flowers-1375015.jpg",
   },
   {
     id: "11",
     title: "S",
-    img: "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
+    src: "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
   },
   {
     id: "12",
     title: "Z",
-    img: "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
+    src: "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
   },
   {
     id: "13",
     title: "A",
-    img: "https://media.istockphoto.com/photos/freedom-chains-that-transform-into-birds-charge-concept-picture-id1322104312?b=1&k=20&m=1322104312&s=170667a&w=0&h=VQyPkFkMKmo0e4ixjhiOLjiRs_ZiyKR_4SAsagQQdkk=",
+    src: "https://media.istockphoto.com/photos/freedom-chains-that-transform-into-birds-charge-concept-picture-id1322104312?b=1&k=20&m=1322104312&s=170667a&w=0&h=VQyPkFkMKmo0e4ixjhiOLjiRs_ZiyKR_4SAsagQQdkk=",
   },
 ];
 
 const Works = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [visible, setVisible] = useState(false);
   return (
     <section>
       <article>
@@ -96,22 +100,38 @@ const Works = () => {
         </nav>
 
         <div className="grid grid-cols-4 grid-flow-row-dense gap-1 md:gap-3 relative m-2">
-          {library.map((data, index, array) => (
-            <div
-              key={data.id}
-              className={`${
-                index % 2 === 0 ? "col-span-1" : "col-span-2"
-              } w-full h-full`}
-            >
-              <Zoom>
+          {library.map((data, index) => {
+            return (
+              <div
+                key={data.id}
+                className={`${
+                  index % 2 === 0 ? "col-span-1" : "col-span-2"
+                } w-full h-full`}
+              >
+                {/* <Zoom> */}
                 <img
-                  src={data.img}
+                  src={data.src}
                   alt="#"
+                  onClick={() => {
+                    setVisible(true);
+                    setActiveIndex(index);
+                  }}
                   className="w-full h-full object-center object-cover rounded"
                 />
-              </Zoom>
-            </div>
-          ))}
+                {/* </Zoom> */}
+              </div>
+            );
+          })}
+          <Viewer
+            visible={visible}
+            onClose={() => {
+              setVisible(false);
+            }}
+            zoomSpeed={0.2}
+            images={library}
+            activeIndex={activeIndex}
+            downloadable
+          />
         </div>
       </article>
     </section>
