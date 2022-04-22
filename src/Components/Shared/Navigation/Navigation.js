fa8672800a5582bc4f051037f1de47484c40aaa7
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2022-04-22 14:16:26 +0600

    arrow animation responsive

--- src/Components/Shared/Navigation/Navigation.js
+++ src/Components/Shared/Navigation/Navigation.js
@@ -14,165 +14,169 @@ const Navigation = () => {
     <section>
       <div className="xl:grid grid-cols-12 gap-x-2 relative z-50">
         <div className="sm:col-span-12 xl:col-span-2 navigation">
-          <div className="bg-white xl:hidden flex justify-between w-full p-5 items-center relative z-50">
-            <div className="flex justify-between  items-center space-x-3">
-              <h1 className="leading-6 text-gray-900">Kabid Hassan</h1>
+          <nav className="relative sticky top-0 z-50">
+            <div className="box-shadow navigation xl:hidden flex justify-between h-[8vh] p-6 items-center bg-white">
+              <div className="flex justify-between  items-center space-x-3">
+                <img
+                  className="rounded-full w-[40px] h-[40px]"
+                  src={man}
+                  alt="kabid Hassan photos"
+                />
+                <p className="text-2xl font-bold leading-6 text-[#FFCF2D]">
+                  Kabid hassan
+                </p>
+              </div>
+              <div
+                aria-label="toggler"
+                className="flex justify-center items-center"
+              >
+                <button
+                  aria-label="open"
+                  id="open"
+                  onClick={() => setShow(true)}
+                  className={`${
+                    show ? "hidden" : ""
+                  } focus:outline-none focus:ring-2`}
+                >
+                  <svg
+                    width={24}
+                    height={24}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4 6H20"
+                      stroke="black"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M4 12H20"
+                      stroke="black"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M4 18H20"
+                      stroke="black"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
+                <button
+                  aria-label="close"
+                  id="close"
+                  onClick={() => setShow(false)}
+                  className={`${
+                    show ? "" : "hidden"
+                  } focus:outline-none focus:ring-2`}
+                >
+                  <svg
+                    width={24}
+                    height={24}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M18 6L6 18"
+                      stroke="black"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M6 6L18 18"
+                      stroke="black"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
             <div
-              aria-label="toggler"
-              className="flex justify-center items-center"
+              id="Main"
+              className={`${
+                show ? "translate-x-0" : "-translate-x-full"
+              } xl: transform  xl:translate-x-0  ease-in-out transition duration-500 flex justify-start items-start h-[92vh]  md:h-[100vh]  w-full sm:w-64  flex-col absolute box-shadow navigation z-50 bg-white `}
             >
-              <button
-                aria-label="open"
-                id="open"
-                onClick={() => setShow(true)}
-                className={`${
-                  show ? "hidden" : ""
-                } focus:outline-none focus:ring-2`}
-              >
-                <svg
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+              <div className="hidden xl:block xl:flex flex-col items-center py-5 mx-5  border-gray-400 border-b">
+                <div className="">
+                  <img
+                    src={man}
+                    alt="Images of Kabid Hossain"
+                    className="img rainbow text-center"
+                  />
+                </div>
+                <Link
+                  to="/home"
+                  className="leading-6 text-4xl font-semibold text-gray-900 text-center py-5"
                 >
-                  <path
-                    d="M4 6H20"
-                    stroke="black"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M4 12H20"
-                    stroke="black"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M4 18H20"
-                    stroke="black"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </button>
-              <button
-                aria-label="close"
-                id="close"
-                onClick={() => setShow(false)}
-                className={`${
-                  show ? "" : "hidden"
-                } focus:outline-none focus:ring-2 text-blue-700`}
-              >
-                <svg
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+                  <span className="">Kabid</span> Hassan
+                </Link>
+              </div>
+              <div className="mt-6 flex flex-col justify-start items-center  pl-4 w-full   space-y-6 pb-5 ">
+                <Link
+                  to="works"
+                  onClick={() => setShow(false)}
+                  className="flex jusitfy-start items-center space-x-6 w-full  focus:outline-none focus:text-[#5046B9]  text-gray-600 rounded "
                 >
-                  <path
-                    d="M18 6L6 18"
-                    stroke="black"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M6 6L18 18"
-                    stroke="black"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-
-          <div
-            id="Main"
-            className={`${
-              show ? "translate-x-0" : "-translate-x-full "
-            }  transform  xl:translate-x-0  ease-in-out transition duration-500  absolute  min-h-screen nav bg-white  md:sticky md:top-0 z-50`}
-          >
-            <div className="hidden xl:block xl:flex flex-col items-center py-5 mx-5  border-gray-400 border-b">
-              <div className="">
-                <img
-                  src={man}
-                  alt="Images of Kabid Hossain"
-                  className="img rainbow text-center"
-                />
-              </div>
-              <Link
-                to="/home"
-                className="leading-6 text-4xl font-semibold text-gray-900 text-center py-5"
-              >
-                <span className="">Kabid</span> Hassan
-              </Link>
-            </div>
-
-            <div className="flex flex-col justify-start items-center  p-5 w-full  space-y-3 pb-5">
-              <NavLink
-                style={({ isActive }) => {
-                  return {
-                    color: isActive ? "#808080" : "",
-                  };
-                }}
-                to="works"
-                className="flex jusitfy-start items-center py-2 space-x-6 w-full  focus:outline-none    text-gray-900 rounded "
-              >
-                Works
-              </NavLink>
-              <NavLink
-                style={({ isActive }) => {
-                  return {
-                    color: isActive ? "#808080" : "",
-                  };
-                }}
-                to="about"
-                className="flex jusitfy-start items-center py-2 space-x-6 w-full  focus:outline-none    text-gray-900 rounded "
-              >
-                About
-              </NavLink>
-              <NavLink
-                style={({ isActive }) => {
-                  return {
-                    color: isActive ? "#808080" : "",
-                  };
-                }}
-                to="contact"
-                className="flex jusitfy-start items-center py-2 space-x-6 w-full  focus:outline-none    text-gray-900 rounded "
-              >
-                Contact
-              </NavLink>
-            </div>
-
-            {/* --------End------- */}
-            <div className="flex flex-col justify-end absolute bottom-0 left-0 items-center h-full p-5  w-full -z-30 ">
-              <div className=" flex justify-between items-center w-full">
-                <Link to="#" className="text-2xl bg p-2 text-[#3b5998]">
-                  <BsGithub />
+                  <p className="text-base text-[18px] font-medium  leading-4 ">
+                    Works
+                  </p>
                 </Link>
-                <Link to="#" className=" text-2xl bg p-2 text-[#3b5998]">
-                  <BsFacebook />
+                <Link
+                  to="contact"
+                  onClick={() => setShow(false)}
+                  className="flex jusitfy-start items-center w-full  space-x-6 focus:outline-none text-gray-600 focus:text-[#5046B9]   rounded "
+                >
+                  <p className="text-base text-[18px] font-medium leading-4 ">
+                    Contact
+                  </p>
                 </Link>
-                <Link to="#" className="text-2xl bg p-2 text-[#bc2a8d]">
-                  <BsInstagram />
-                </Link>
-                <Link to="#" className=" text-2xl bg p-2 text-[#c8232c]">
-                  <BsPinterest />
+                <Link
+                  to="about"
+                  onClick={() => setShow(false)}
+                  className="flex jusitfy-start items-center space-x-6 w-full  focus:outline-none  focus:text-[#5046B9]  text-gray-600 rounded "
+                >
+                  <p className="text-base text-[18px] font-medium leading-4  ">
+                    About
+                  </p>
                 </Link>
               </div>
-              <div className="text-center mt-5 text-sm text-gray-400">
-                <p>Copyright © 2022 Kabid Hassan</p>
+
+              <div className="flex flex-col justify-end items-center h-full pb-6  px-6  w-full">
+                <div className=" flex justify-center items-center w-full space-x-2">
+                  <div className="flex flex-col justify-between items-center  space-x-2">
+                    <div className=" flex justify-between items-center w-full">
+                      <Link to="#" className="text-2xl bg p-2 text-[#3b5998]">
+                        <BsGithub />
+                      </Link>
+                      <Link to="#" className=" text-2xl bg p-2 text-[#3b5998]">
+                        <BsFacebook />
+                      </Link>
+                      <Link to="#" className="text-2xl bg p-2 text-[#bc2a8d]">
+                        <BsInstagram />
+                      </Link>
+                      <Link to="#" className=" text-2xl bg p-2 text-[#c8232c]">
+                        <BsPinterest />
+                      </Link>
+                    </div>
+                    <div className="text-center mt-5 text-sm text-gray-400">
+                      <p>Copyright © 2022 Kabid Hassan</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          </nav>
         </div>
         <div className="col-span-10">
           <Routes>
