6c7d31b4860bae1a13f0fd8e045761df6efd1f54
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2022-04-22 12:52:39 +0600

    ss

--- src/Components/Home/About/About.js
+++ src/Components/Home/About/About.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { Fade, Zoom } from "react-reveal";
+import Fade from "react-reveal/Fade";
 import "./About.css";
 import img1 from "../../Shared/Images/about2.png";
 import img2 from "../../Shared/Images/about3.png";
 import img3 from "../../Shared/Images/about4.png";
 import { MdDoubleArrow } from "react-icons/md";
 import { TiArrowForward } from "react-icons/ti";
+import { Link } from "react-router-dom";
+import { Zoom } from "react-reveal";
 
 const About = () => {
   return (
     <article>
       <section className="c-about md:px-10">
-        <nav className="c-header z-40">
+        <nav className="c-header">
           <h1 className="">
             <span className=" uppercase">About Me!😎</span>
           </h1>
@@ -19,33 +21,88 @@ const About = () => {
 
         <div className="c-header-details relative">
           <div className="md:w-[600px] py-5 border-gray-400 border-b">
-            <h1>About Me!😎</h1>
-            <p>
-              Good at! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Magni illum minus officia minima sunt eveniet dignissimos
-            </p>
+            <Zoom>
+              <h1>About Me!😎</h1>
+            </Zoom>
+            <Fade bottom cascade>
+              <p>
+                Good at! Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Magni illum minus officia minima sunt eveniet dignissimos
+              </p>
+            </Fade>
           </div>
-          <div className="bottom-arrow-icon">
-            {/* <p className="text-3xl">Good At</p> */}
-            <h1>
-              <TiArrowForward />
-            </h1>
-          </div>
+          <Zoom>
+            <div className="bottom-arrow-icon">
+              <h1>
+                <TiArrowForward />
+              </h1>
+            </div>
+          </Zoom>
         </div>
         <div>
-          <div className="grid grid-cols-2 md:min-h-[540px] p-10 relative">
-            <div className="col-span-1 h-auto relative flex justify-evenly items-center mx-5">
-              <div className="w-full flex flex-col justify-evenly items-center ">
-                <div className="a-skill">
-                  <p>Illustrator </p>
+          <div className="grid grid-cols-2 md:min-h-[540px] md:p-10 relative">
+            <div className="col-span-2 order-last md:order-first md:col-span-1 h-auto relative flex justify-evenly items-center mx-5">
+              <Fade bottom cascade>
+                <div className="w-full flex flex-col justify-evenly items-center ">
+                  <Link to="#" className="a-skill">
+                    <p>Illustrator </p>
+                  </Link>
+                  <Link to="#" className="a-skill">
+                    <p>Abstract Artist</p>
+                  </Link>
+                  <Link to="#" className="a-skill">
+                    <p>Designer</p>
+                  </Link>
                 </div>
-                <div className="a-skill">
-                  <p>Abstract Artist</p>
-                </div>
-                <div className="a-skill">
-                  <p>Designer</p>
-                </div>
+              </Fade>
+
+              <div className="a-bottom-icon">
+                <h1>
+                  <Zoom>
+                    <MdDoubleArrow />
+                  </Zoom>
+                </h1>
               </div>
+            </div>
+            <div className="col-span-2 md:col-span-1 flex justify-center items-center">
+              <Zoom>
+                <img
+                  src={img1}
+                  alt=""
+                  width="100%"
+                  height="100%"
+                  className="rounded-full"
+                />
+              </Zoom>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 md:min-h-[540px] mt-20 md:mt-0 md:p-10 relative">
+            <div className="col-span-2 md:col-span-1  flex justify-center items-center">
+              <Zoom>
+                <img
+                  src={img2}
+                  alt=""
+                  width="100%"
+                  height="100%"
+                  className="rounded-full"
+                />
+              </Zoom>
+            </div>
+            <div className="col-span-2 md:col-span-1 h-auto relative flex justify-evenly items-center mx-5">
+              <Fade bottom cascade>
+                <div className="w-full flex flex-col justify-evenly bg-transparent items-center ">
+                  <Link to="#" className="a-skill">
+                    <p>Researcher</p>
+                  </Link>
+                  <Link to="#" className="a-skill">
+                    <p>Educator</p>
+                  </Link>
+                  <Link to="#" className="a-skill">
+                    <p>Content Creator</p>
+                  </Link>
+                </div>
+              </Fade>
 
               <div className="a-bottom-icon">
                 <h1>
@@ -53,63 +110,29 @@ const About = () => {
                 </h1>
               </div>
             </div>
-            <div className="col-span-1 flex justify-center items-center">
-              <img
-                src={img1}
-                alt=""
-                width="100%"
-                height="100%"
-                className="rounded-full"
-              />
-            </div>
           </div>
-          <div className="grid grid-cols-2 md:min-h-[540px] p-10 relative">
-            <div className="col-span-1 flex justify-center items-center">
-              <img
-                src={img2}
-                alt=""
-                width="100%"
-                height="100%"
-                className="rounded-full"
-              />
-            </div>
-            <div className="col-span-1 h-auto relative flex justify-evenly items-center mx-5">
-              <div className="w-full flex flex-col justify-evenly items-center ">
-                <div className="a-skill">
-                  <p>Illustrator </p>
-                </div>
-                <div className="a-skill">
-                  <p>Abstract Artist</p>
-                </div>
-                <div className="a-skill">
-                  <p>Designer</p>
-                </div>
-              </div>
 
-              <div className="a-bottom-icon">
-                <h1>
-                  <MdDoubleArrow />
-                </h1>
+          <div className="grid grid-cols-2 md:min-h-[540px] mt-20 md:mt-0 md:p-10 relative">
+            <div className="col-span-2 order-last md:order-first md:col-span-1 h-auto relative flex justify-evenly items-center mx-5 bg-transparent">
+              <div className="w-full flex flex-col justify-evenly bg-transparent items-center ">
+                <Fade bottom cascade>
+                  <div className="shape bg-transparent">
+                    <p className="">Professional Biriyani Lover!😎</p>
+                  </div>
+                </Fade>
               </div>
             </div>
-          </div>
-          <div className="grid grid-cols-2  md:min-h-[540px] p-10 relative">
-            <div className="col-span-1 h-auto relative flex justify-evenly items-center mx-5">
-              <div className="w-full flex flex-col justify-evenly items-center ">
-                <div className="shape">
-                  <p className="">Professional Biriyani Lover!😎</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-span-1 flex justify-center items-center">
+            <div className="col-span-2  md:col-span-1  flex justify-center items-center">
               {" "}
-              <img
-                src={img3}
-                alt=""
-                width="100%"
-                height="100%"
-                className="rounded-full"
-              />
+              <Zoom>
+                <img
+                  src={img3}
+                  alt=""
+                  width="100%"
+                  height="100%"
+                  className="rounded-full"
+                />
+              </Zoom>
             </div>
           </div>
         </div>
--- src/Components/Home/Contact/Contact.js
+++ src/Components/Home/Contact/Contact.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { BsFacebook, BsGithub, BsInstagram, BsPinterest } from "react-icons/bs";
 import { ImLocation2, ImMail3, ImPhone } from "react-icons/im";
 import { TiArrowForward } from "react-icons/ti";
+import { Fade, Zoom } from "react-reveal";
 import { Link } from "react-router-dom";
 import contactMe2 from "../../Shared/Images/contact-2.png";
 import contactMe from "../../Shared/Images/contact.png";
@@ -51,143 +52,158 @@ const Contact = () => {
       <div className="c-contact">
         <div className="c-header-details">
           <div className="md:w-[600px] py-5 border-gray-400 border-b">
-            <h1>DON'T BE SHY</h1>
-            <p>
-              Feel free to get in touch with me, i'm always open to discussing
-              new projects creative ideas or opportunities to be part of your
-              vision
-            </p>
+            <Zoom>
+              <h1>DON'T BE SHY</h1>
+            </Zoom>
+            <Fade bottom cascade>
+              <p>
+                Feel free to get in touch with me, i'm always open to discussing
+                new projects creative ideas or opportunities to be part of your
+                vision
+              </p>
+            </Fade>
           </div>
         </div>
         <div className="relative grid md:grid-cols-2 items-center justify-center  z-40">
           <div className="col-span-1 order-last md:order-first p-5">
-            <div class="md:p-6 flex flex-col justify-center">
-              <div className="mb-5 text-3xl">
-                <p>
-                  Drop Us a <span className="">Massage!</span>
-                </p>
+            <Fade bottom cascade>
+              <div class="md:p-6 flex flex-col justify-center">
+                <div className="mb-5 text-3xl">
+                  <p>
+                    Drop Us a <span className="">Massage!</span>
+                  </p>
+                </div>
+
+                <form className="z-40 bg-transparent" onSubmit={sendEmail}>
+                  <div class="flex flex-col">
+                    <label htmlFor="name" class="hidden">
+                      Full Name
+                    </label>
+                    <input
+                      type="name"
+                      name="name"
+                      id="name"
+                      placeholder="Full Name"
+                      class="w-100 mt-2 py-2 px-3 rounded    c-shadow min-h-full text-gray-800 focus:outline-none bg-transparent"
+                    />
+                  </div>
+
+                  <div class="flex flex-col mt-2">
+                    <label htmlFor="email" class="hidden">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="Email"
+                      class="w-100 mt-2 py-2 px-3 rounded    c-shadow min-h-full text-gray-800 focus:outline-none bg-transparent"
+                    />
+                  </div>
+
+                  <div class="flex flex-col mt-2">
+                    <label htmlFor="text" class="hidden">
+                      Text
+                    </label>
+                    <textarea
+                      type="text"
+                      name="message"
+                      id="text"
+                      placeholder="Type your message"
+                      class="w-100 mt-2 py-2 px-3 min-h-[200px] rounded    c-shadow min-h-full text-gray-800 focus:outline-none bg-transparent"
+                    />
+                  </div>
+
+                  <button
+                    type="submit"
+                    class="w-full c-shadow  bg-[#808080] text-white font-bold py-3 px-6 rounded mt-3 hover:bg-[#5a5a5a]  transition ease-in-out duration-300"
+                  >
+                    Send
+                  </button>
+                </form>
               </div>
-
-              <form className="z-40 bg-transparent" onSubmit={sendEmail}>
-                <div class="flex flex-col">
-                  <label htmlFor="name" class="hidden">
-                    Full Name
-                  </label>
-                  <input
-                    type="name"
-                    name="name"
-                    id="name"
-                    placeholder="Full Name"
-                    class="w-100 mt-2 py-2 px-3 rounded    c-shadow min-h-full text-gray-800 focus:outline-none bg-transparent"
-                  />
-                </div>
-
-                <div class="flex flex-col mt-2">
-                  <label htmlFor="email" class="hidden">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="Email"
-                    class="w-100 mt-2 py-2 px-3 rounded    c-shadow min-h-full text-gray-800 focus:outline-none bg-transparent"
-                  />
-                </div>
-
-                <div class="flex flex-col mt-2">
-                  <label htmlFor="text" class="hidden">
-                    Text
-                  </label>
-                  <textarea
-                    type="text"
-                    name="message"
-                    id="text"
-                    placeholder="Type your message"
-                    class="w-100 mt-2 py-2 px-3 min-h-[200px] rounded    c-shadow min-h-full text-gray-800 focus:outline-none bg-transparent"
-                  />
-                </div>
-
-                <button
-                  type="submit"
-                  class="w-full c-shadow  bg-[#808080] text-white font-bold py-3 px-6 rounded mt-3 hover:bg-[#5a5a5a]  transition ease-in-out duration-300"
-                >
-                  Send
-                </button>
-              </form>
+            </Fade>
+          </div>
+          <Fade bottom cascade>
+            <div className="col-span-1  p-5">
+              <img src={contactMe} width="100%" alt="" />
             </div>
-          </div>
-          <div className="col-span-1  p-5">
-            <img src={contactMe} width="100%" alt="" />
-          </div>
-          <div className="c-bottom-icon">
-            <h1>
-              <TiArrowForward />
-            </h1>
-            <p className="text-3xl">Or You Can Find Me</p>
-          </div>
+          </Fade>
+          <Zoom>
+            <div className="c-bottom-icon">
+              <h1>
+                <TiArrowForward />
+              </h1>
+
+              <p className="text-3xl">Or You Can Find Me</p>
+            </div>
+          </Zoom>
         </div>
         <div className="mt-32 md:mt-20">
           <div className="md:grid grid-cols-2 items-center justify-center  z-50">
-            <div className="col-span-1 p-5">
-              <img src={contactMe2} width="100%" alt="" />
-            </div>
-            <div className="col-span-1 p-5 z-40">
-              <div class="md:p-6 flex flex-col justify-center c-address-section">
-                <address className="c-address">
-                  <p className="c-address-icon">
-                    <ImLocation2 />
-                  </p>
-                  <p>
-                    Box 564, Disneyland,
-                    <br />
-                    Dhaka, Bangladesh
-                  </p>
-                </address>
-                <div className="c-address">
-                  <p className="c-address-icon">
-                    <ImPhone />
-                  </p>
-                  <div>
-                    <p>+88 0534684646</p>
-                    <p>+00 8893513546</p>
-                  </div>
-                </div>
-                <div className="c-address">
-                  <p className="c-address-icon">
-                    <ImMail3 />
-                  </p>
-                  <p>kabidhasan@gmail.com</p>
-                </div>
-                <div>
-                  <div className="flex justify-evenly m-[15px]">
-                    <Link
-                      to="#"
-                      className="text-2xl text-[#808080] bg p-2 hover:text-[#5a5a5a]  transition ease-in-out duration-300"
-                    >
-                      <BsGithub />
-                    </Link>
-                    <Link
-                      to="#"
-                      className=" text-2xl text-[#808080] bg p-2 hover:text-[#3b5998] transition ease-in-out duration-300"
-                    >
-                      <BsFacebook />
-                    </Link>
-                    <Link
-                      to="#"
-                      className="text-2xl text-[#808080] bg p-2 hover:text-[#bc2a8d] transition ease-in-out duration-300"
-                    >
-                      <BsInstagram />
-                    </Link>
-                    <Link
-                      to="#"
-                      className=" text-2xl text-[#808080] bg p-2 hover:text-[#c8232c] transition ease-in-out duration-300"
-                    >
-                      <BsPinterest />
-                    </Link>
-                  </div>
-                </div>
+            <Fade bottom cascade>
+              <div className="col-span-1 p-5">
+                <img src={contactMe2} width="100%" alt="" />
               </div>
+            </Fade>
+            <div className="col-span-1 p-5 z-40">
+              <Fade bottom cascade>
+                <div class="md:p-6 flex flex-col justify-center c-address-section">
+                  <address className="c-address">
+                    <p className="c-address-icon">
+                      <ImLocation2 />
+                    </p>
+                    <p>
+                      Box 564, Disneyland,
+                      <br />
+                      Dhaka, Bangladesh
+                    </p>
+                  </address>
+                  <div className="c-address">
+                    <p className="c-address-icon">
+                      <ImPhone />
+                    </p>
+                    <div>
+                      <p>+88 0534684646</p>
+                      <p>+00 8893513546</p>
+                    </div>
+                  </div>
+                  <div className="c-address">
+                    <p className="c-address-icon">
+                      <ImMail3 />
+                    </p>
+                    <p>kabidhasan@gmail.com</p>
+                  </div>
+                  <div>
+                    <div className="flex justify-evenly m-[15px]">
+                      <Link
+                        to="#"
+                        className="text-2xl text-[#808080] bg p-2 hover:text-[#5a5a5a]  transition ease-in-out duration-300"
+                      >
+                        <BsGithub />
+                      </Link>
+                      <Link
+                        to="#"
+                        className=" text-2xl text-[#808080] bg p-2 hover:text-[#3b5998] transition ease-in-out duration-300"
+                      >
+                        <BsFacebook />
+                      </Link>
+                      <Link
+                        to="#"
+                        className="text-2xl text-[#808080] bg p-2 hover:text-[#bc2a8d] transition ease-in-out duration-300"
+                      >
+                        <BsInstagram />
+                      </Link>
+                      <Link
+                        to="#"
+                        className=" text-2xl text-[#808080] bg p-2 hover:text-[#c8232c] transition ease-in-out duration-300"
+                      >
+                        <BsPinterest />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </Fade>
             </div>
           </div>
         </div>
