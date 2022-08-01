c4f6ebd4278648b322f990728deac8d664ee466b
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2022-08-01 12:09:42 +0600

    sc

--- src/Components/Works/Art.js
+++ src/Components/Works/Art.js
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import { Zoom } from "react-reveal";
 import Viewer from "react-viewer";
 import { getArt } from "../Hooks/getApi";
 import Spinner from "../Utils/Spinner";
+import SrcBar from "../Utils/SrcBar";
 import { pageTitle } from "../Utils/Title";
 
 import "./Works.css";
@@ -13,11 +14,15 @@ import "./Works.css";
 
 const Works = () => {
   pageTitle("Art");
-  const [item, setItem] = useState("");
-  console.log(item);
+
+  const [post, setPost] = useState([]);
+  const [srcResult, setSrcResult] = useState([]);
 
   const { data, isLoading, isError, error } = useQuery(["art"], () => getArt());
-
+  useEffect(() => {
+    setPost(data);
+    setSrcResult(data);
+  }, [data]);
   const [activeIndex, setActiveIndex] = useState(0);
   const [visible, setVisible] = useState(false);
 
@@ -39,67 +44,52 @@ const Works = () => {
   return (
     <section>
       <article>
-        <nav className="flex justify-center items-center m-2 rounded md:sticky md:top-2 z-50 absolute px-2">
-          {/* <h1 className=" py-3 uppercase">Recent Works </h1>
-           */}
-          <div className="search-box shadow hidden md:block">
-            <input
-              className="search-input"
-              type="text"
-              name=""
-              onChange={(e) => setItem(e.target.value)}
-              placeholder="Search by Artworks, gallery, theme.."
-            />
-            <button type="submit" className="search-btn">
-              <p className="fa-search">
-                <BsSearch />
-              </p>
-            </button>
+        <nav className="px-2 w-full md:flex justify-center items-center">
+          <div className=" sticky top-0  z-40 pt-2">
+            <div className="md:w-[450px]">
+              <SrcBar post={post} setSrcResult={setSrcResult} />
+            </div>
           </div>
         </nav>
 
-        <div className="grid grid-cols-4 grid-flow-row-dense gap-1 md:gap-3 relative m-2">
-          {data
-            ?.filter((art) =>
-              art?.title.toLowerCase().includes(item.toLowerCase())
-            )
-            .map((data, index) => {
-              return (
-                <Zoom>
-                  <div
-                    key={data?._id}
-                    className={`${
-                      index % 2 === 0
-                        ? "col-span-4 md:col-span-1"
-                        : "col-span-4 md:col-span-2"
-                    } w-full h-full max-h-[450px] art-section  bg-transparent`}
-                    onClick={() => {
-                      setVisible(true);
-                      setActiveIndex(index);
-                    }}
-                  >
-                    <img
-                      src={data?.art_img}
-                      alt="#"
-                      className="w-full h-full object-center object-cover aspect-video  art-images "
-                    />
+        <div className="grid grid-cols-4 grid-flow-row-dense gap-2 md:gap-3 relative m-2">
+          {srcResult?.map((data, index) => {
+            return (
+              <Zoom>
+                <div
+                  key={data?._id}
+                  className={`${
+                    index % 2 === 0
+                      ? "col-span-4 md:col-span-1"
+                      : "col-span-4 md:col-span-2"
+                  } w-full h-full max-h-[450px] art-section  bg-transparent`}
+                  onClick={() => {
+                    setVisible(true);
+                    setActiveIndex(index);
+                  }}
+                >
+                  <img
+                    src={data?.art_img}
+                    alt="#"
+                    className="w-full h-full object-center object-cover aspect-video  art-images "
+                  />
 
-                    <div class="">
-                      <p class="absolute bottom-[5px] left-2/4 -translate-x-2/4 w-full text-center  text-md text">
-                        {data.title}
-                      </p>
-                    </div>
+                  <div class="">
+                    <p class="absolute bottom-[5px] left-2/4 -translate-x-2/4 w-full text-center  text-md text">
+                      {data.title}
+                    </p>
                   </div>
-                </Zoom>
-              );
-            })}
+                </div>
+              </Zoom>
+            );
+          })}
           <Viewer
             visible={visible}
             onClose={() => {
               setVisible(false);
             }}
             zoomSpeed={0.2}
-            images={data}
+            images={srcResult}
             activeIndex={activeIndex}
             downloadable
           />
